9622b5cc5eda8b4782f3be20d87d69c263372925
angel <CAngel080803@gmail.com>
2025-08-05 00:40:14 -0400

    testing

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,25 @@
+// import React from 'react';
+import { useState } from 'react';
+import Navbar from '../../components/navbar/Navbar.jsx';
+import Sidebar from '../../components/navbar/Sidebar.jsx';
+import Footer from '../../components/footer/Footer.jsx';
+import FadeIn from '../../components/fadeinsection/FadeIn.jsx';
+
+function Home() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => setSidebarOpen(!sidebarOpen);
+
+  return (
+    <>
+      <Navbar toggleSidebar={toggleSidebar} />
+      <FadeIn>
+      <Sidebar isOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
+      
+      </FadeIn>
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
