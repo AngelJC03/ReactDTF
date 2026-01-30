e573f5d86aae609ddc49dd41706e6e31ad6a2e17
angel <CAngel080803@gmail.com>
2026-01-29 19:54:20 -0500

    entire news article cell is not a hyuperlink

--- src/pages/AboutUs/InTheNews/Articles.jsx
+++ src/pages/AboutUs/InTheNews/Articles.jsx
@@ -70,11 +70,13 @@ function Articles() {
               <img src={imageUrl} alt="Article Image" />
             </div>
             <div className="content">
-              <div className="date">{formattedDate}</div>
-              <div className="title" role="heading">
-                <a href={link}>{title}</a>
-              </div>
-              <div className="description">{truncatedDescription}</div>
+              <a href={link} className="article-cell">
+                <div className="date">{formattedDate}</div>
+                <div className="title" role="heading">
+                  <a href={link}>{title}</a>
+                </div>
+                <div className="description">{truncatedDescription}</div>
+              </a>
             </div>
           </div>
         );
